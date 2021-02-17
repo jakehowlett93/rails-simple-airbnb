1711ea3f466c01658016983857f50de713803916
jake howlett <jake.howlett93@gmail.com>
2021-02-17 15:24:42 +0000

    install faker gem and seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing database"
+Flat.destroy_all
+
+puts "Generating flats.."
+5.times do
+  Flat.create(
+    name: Faker::FunnyName.two_word_name,
+    address: Faker::Address.full_address,
+    description: Faker::Books::Lovecraft.sentence,
+    price_per_night: [10, 15, 50, 65, 150].sample,
+    number_of_guests: [1, 2, 3, 4, 5, 6, 7, 8].sample
+  )
+end
