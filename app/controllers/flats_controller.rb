9ac6335ab48e609a3f1f282702de3b62588fd43f
jake howlett <jake.howlett93@gmail.com>
2021-02-17 16:20:02 +0000

    add create method and some styling

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,4 +10,19 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :price_per_night, :number_of_guests)
+  end
 end
